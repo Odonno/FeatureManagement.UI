904b8a844ea1002c675e94ea5fea4a0c316b1f78
Odonno <bottiau.david@laposte.net>
2022-02-19 17:42:05 +0100

    ♻️ create Background layout component

--- Source/features-ui/src/routes/__layout.svelte
+++ Source/features-ui/src/routes/__layout.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
 	import 'fluent-svelte/theme.css';
 	import NavBar from '$components/layout/NavBar.svelte';
-	import { onMount } from 'svelte';
-	import bgLight from '$assets/bloom-mica-light.png';
-	import bgDark from '$assets/bloom-mica-dark.png';
-
-	const applyBackground = (colorScheme: 'dark' | 'light') => {
-		const backgroundElement = document.getElementsByClassName('background')[0] as HTMLDivElement;
-
-		if (colorScheme === 'dark') {
-			backgroundElement.style.backgroundImage = `url(${bgDark})`;
-		} else {
-			backgroundElement.style.background = `url(${bgLight}) center/170% no-repeat fixed`;
-		}
-	};
-
-	onMount(() => {
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
-			const colorScheme = e.matches ? 'dark' : 'light';
-			applyBackground(colorScheme);
-		});
-
-		const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-			? 'dark'
-			: 'light';
-		applyBackground(colorScheme);
-	});
+	import Background from '$components/layout/Background.svelte';
 </script>
 
-<div class="background" />
+<Background />
 
 <NavBar />
 
@@ -129,15 +105,6 @@
 		:global(svg) {
 			fill: var(--fds-text-primary);
 		}
-	}
-
-	.background {
-		position: fixed;
-		z-index: -1;
-		width: 100vw;
-		height: 100vh;
-
-		box-shadow: inset 0 0 0 100vmax var(--fds-card-background-secondary);
 	}
 
 	main {
